fcac1d26d16d147ed017df238d78726c6a41d819
felopateershokry <143431396+felopateershokry@users.noreply.github.com>
2025-09-25 22:55:34 +0300

    first step

---
+++ client/src/pages/Student/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hero from '../../components/Student/Hero'
+
+const Home = () => {
+    return (
+        <div className='flex flex-col items-center space-y-10 text-center'>
+            <Hero />
+        </div>
+    )
+}
+
+export default Home
